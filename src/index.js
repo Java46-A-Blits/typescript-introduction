c9e75739f51f6c8c5e6ca43397ba8143e5ba2ebd
Alex Blits <blits.alex@gmail.com>
2023-01-25 13:25:37 +0200

    HW32-Set, Map

--- src/index.js
+++ src/index.js
@@ -19,10 +19,10 @@
 // function sum(ar: number[]): number{
 //     return ar.reduce((res, cur)=> res+cur, 0)
 // }
-// let  ar: Array<number>= [1,2,3,10];
+let ar = [1, 2, 3, 10];
 // ar.push(12);
-// console.log(sum(ar));
-// let set = new Set<number>([1,1,1,2,2,3,3]);
+// console.log(ar);
+let set = new Set([1, 1, 1, 2, 2, 3, 3]);
 // console.log(set); // will  show only unique numbers: Set(3) { 1, 2, 3 },  set can't have the same entry values
 // set.forEach(e=> console.log(e));
 // set.add(10);
@@ -31,15 +31,17 @@
 // console.log (set.has(10)); 
 //// +++++++++++   HW 32 (sets) ++++++++++++++++++++++++++++
 function intersection(set1, set2) {
-    //TODO write function returning array of common numbers between two sets
+    //write function returning array of common numbers between two sets
     //that are  the numbers existing in both sets
-    return [];
+    return Array.from(set1).filter(n => set2.has(n));
 }
 function subtract(set1, set2) {
-    //TODO write function returning array of numbers from set1 that don't exist in the 
+    //write function returning array of numbers from set1 that don't exist in the 
     //set2
-    return [];
+    return Array.from(set1).filter(n => !set2.has(n));
 }
+let set2 = new Set([1, 2, 3, 4, 5, 6]);
+let set1 = new Set([1, 2, 3, 6, 7]);
 function getSortedOccurences(array) {
     //write function returning array of occurrences 
     //each occurrency contains a string from the given array and how many times it occures in the array
@@ -47,18 +49,28 @@ function getSortedOccurences(array) {
     //example: the given array is ['lmn', 'ab', 'a', 'cd', 'lmn', 'cd', 'lmn']
     //result: [{str: 'lmn', count: 3}, {str: 'cd', count: 2}, {str: 'a', count:1}, {str: 'ab', count:1}]
     //implementation notes: to use Map<string, number>
-    return [];
+    const mapOccurence = array.reduce((map, str) => {
+        var _a;
+        const count = (_a = map.get(str)) !== null && _a !== void 0 ? _a : 0;
+        map.set(str, count + 1);
+        return map;
+    }, new Map());
+    return Array.from(mapOccurence)
+        .map(e => ({ str: e[0], count: e[1] }))
+        .sort((o1, o2) => o2.count - o1.count || o1.str.localeCompare(o2.str));
 }
+let arr = ['lmn', 'ab', 'a', 'cd', 'lmn', 'cd', 'lmn'];
+// console.log(getSortedOccurences(arr));//--> [{ str: 'lmn', count: 3 },{ str: 'cd', count: 2 },{ str: 'a', count: 1 },{ str: 'ab', count: 1 }]
 //+++++++++++++++++++++++++++++++++++++
 // (MAPS)
 const mapMonths = new Map([
     [1, 'Jan'], [2, 'Feb'], [3, 'Mar'] // ['GG', 'YY'] wil  give an  error
 ]);
 mapMonths.set(4, 'Apr');
-mapMonths.set(1, 'JAN'); // 'set' method will change(add) prevous(new) entry
+mapMonths.set(1, 'JAN'); // 'set' method will change(add) previous(new) entry
 // console.log(mapMonths);//         --> Map(4) { 1 => 'JAN', 2 => 'Feb', 3 => 'Mar', 4 => 'Apr' }
-// console.log(mapMonths.get(1));//  --> "JAN"
-// console.log(mapMonths.has(2));//  --> 'true'
+// console.log(mapMonths.get(1));//  --> "JAN" --- 'get' getting the the value of the given key!
+// console.log(mapMonths.has(2));//  --> 'true' -- 'has' looking at the KEY !!!
 const arEntries = Array.from(mapMonths); // creating an array of entries
 // console.log(arEntries); //        --> [ [ 1, 'JAN' ], [ 2, 'Feb' ], [ 3, 'Mar' ], [ 4, 'Apr' ] ]
 const arKeys = Array.from(mapMonths.keys()); // getting an  array of keys
@@ -72,10 +84,22 @@ function createPerson(id, name) {
 }
 // console.log(createPerson(10, 'kuku')); // --> { id: 10, name: 'kuku' }
 let person = { id: 100, name: 'Alex', age: 40, city: "Haifa" };
+let person2 = { id: 101, name: 'Felix', age: 31, city: 'TA' };
 let persons = []; // HAVE TO  DEFINE ARRAY THIS WAY  !!!!!!!!!
 persons.push(person);
 // console.log(persons); // --> [ { id: 100, name: 'Alex', age: 40, city: 'Haifa' } ]
+persons.push(person2);
+// console.log(persons);  // -->  [{ id: 100, name: 'Alex', age: 40, city: 'Haifa' }, { id: 101, name: 'Felix', age: 31, city: 'TA' }]
 function strLength(str) {
     return str.length;
 }
 // console.log(strLength(person.city));  //  -->  5
+/// =====  GENERICS =====
+// interface Backpack<Type> {
+//     add: (obj: Type)=> void;
+//     get: ()=> Type;
+// }
+// declare const backpack: Backpack<string>;// if no 'declare' ERR -> "const' declarations must be initialized", used as a
+// const obj =backpack.get();               // shutter to  TS inlline compilation
+// // backpack.add(23); //since backpack variable is a string it will  pop an error
+//                      // "Argument of type 'number' is not assignable to parameter of type 'string'
--- src/index.ts
+++ src/index.ts
@@ -19,10 +19,10 @@
 // function sum(ar: number[]): number{
 //     return ar.reduce((res, cur)=> res+cur, 0)
 // }
-// let  ar: Array<number>= [1,2,3,10];
+let  ar: Array<number>= [1,2,3,10];
 // ar.push(12);
-// console.log(sum(ar));
-// let set = new Set<number>([1,1,1,2,2,3,3]);
+// console.log(ar);
+let set = new Set<number>([1,1,1,2,2,3,3]);
 // console.log(set); // will  show only unique numbers: Set(3) { 1, 2, 3 },  set can't have the same entry values
 // set.forEach(e=> console.log(e));
 // set.add(10);
@@ -33,28 +33,42 @@
 //// +++++++++++   HW 32 (sets) ++++++++++++++++++++++++++++
 
 function intersection(set1: Set<number>, set2: Set<number>): number[]{
-        //TODO write function returning array of common numbers between two sets
+        //write function returning array of common numbers between two sets
         //that are  the numbers existing in both sets
-    return[];
+    return Array.from(set1).filter(n=> set2.has(n))
 }
 function subtract(set1: Set<number>, set2: Set<number>): number[]{
-    //TODO write function returning array of numbers from set1 that don't exist in the 
-    //set2
-    return[];
+         //write function returning array of numbers from set1 that don't exist in the 
+         //set2
+    return Array.from(set1).filter(n=> !set2.has(n))
 }
+let set2 = new Set<number>([1,2,3,4,5,6]);
+let set1 = new Set<number>([1,2,3,6,7]);
+// console.log(intersection(set1,set2))  //--> [ 1, 2, 3, 6 ]
+// console.log(subtract(set1,set2));     //--> [7]
+
 type Occurence = {
     str: string; 
     count: number;
 }
 function getSortedOccurences(array: string[]): Occurence[]{
-    //write function returning array of occurrences 
-    //each occurrency contains a string from the given array and how many times it occures in the array
-    // a result array should be sorted in the descending order of the occurrences and ascending order of the strings
-    //example: the given array is ['lmn', 'ab', 'a', 'cd', 'lmn', 'cd', 'lmn']
-    //result: [{str: 'lmn', count: 3}, {str: 'cd', count: 2}, {str: 'a', count:1}, {str: 'ab', count:1}]
-    //implementation notes: to use Map<string, number>
-    return [];
+          //write function returning array of occurrences 
+            //each occurrency contains a string from the given array and how many times it occures in the array
+            // a result array should be sorted in the descending order of the occurrences and ascending order of the strings
+            //example: the given array is ['lmn', 'ab', 'a', 'cd', 'lmn', 'cd', 'lmn']
+            //result: [{str: 'lmn', count: 3}, {str: 'cd', count: 2}, {str: 'a', count:1}, {str: 'ab', count:1}]
+            //implementation notes: to use Map<string, number>
+            const mapOccurence =  array.reduce((map,str)=> {
+                const count = map.get(str) ?? 0;
+                map.set(str, count+1);
+                return map;
+            }, new Map<string, number>());
+            return Array.from(mapOccurence)
+            .map(e=> ({str: e[0], count: e[1]}))
+            .sort((o1, o2)=> o2.count - o1.count || o1.str.localeCompare(o2.str));            
 }
+let arr = ['lmn', 'ab', 'a', 'cd', 'lmn', 'cd', 'lmn'];
+// console.log(getSortedOccurences(arr));//--> [{ str: 'lmn', count: 3 },{ str: 'cd', count: 2 },{ str: 'a', count: 1 },{ str: 'ab', count: 1 }]
 
 //+++++++++++++++++++++++++++++++++++++
 // (MAPS)
@@ -62,10 +76,10 @@ const mapMonths = new Map<number, string>([
     [1,'Jan'],[2,'Feb'],[3,'Mar']  // ['GG', 'YY'] wil  give an  error
 ]);
 mapMonths.set(4,'Apr');
-mapMonths.set(1,'JAN') // 'set' method will change(add) prevous(new) entry
+mapMonths.set(1,'JAN') // 'set' method will change(add) previous(new) entry
 // console.log(mapMonths);//         --> Map(4) { 1 => 'JAN', 2 => 'Feb', 3 => 'Mar', 4 => 'Apr' }
-// console.log(mapMonths.get(1));//  --> "JAN"
-// console.log(mapMonths.has(2));//  --> 'true'
+// console.log(mapMonths.get(1));//  --> "JAN" --- 'get' getting the the value of the given key!
+// console.log(mapMonths.has(2));//  --> 'true' -- 'has' looking at the KEY !!!
 const arEntries = Array.from(mapMonths); // creating an array of entries
 // console.log(arEntries); //        --> [ [ 1, 'JAN' ], [ 2, 'Feb' ], [ 3, 'Mar' ], [ 4, 'Apr' ] ]
 const arKeys = Array.from(mapMonths.keys()); // getting an  array of keys
@@ -87,20 +101,27 @@ function createPerson(id: number, name: string): {id: number, name: string}{
 }
 // console.log(createPerson(10, 'kuku')); // --> { id: 10, name: 'kuku' }
 let person: Person = {id: 100, name: 'Alex', age: 40, city: "Haifa"}
+let person2: Person = {id:101, name: 'Felix', age: 31, city: 'TA'}
 let persons: Person[] = [];   // HAVE TO  DEFINE ARRAY THIS WAY  !!!!!!!!!
 persons.push(person)  ;
 // console.log(persons); // --> [ { id: 100, name: 'Alex', age: 40, city: 'Haifa' } ]
+persons.push(person2);
+// console.log(persons);  // -->  [{ id: 100, name: 'Alex', age: 40, city: 'Haifa' }, { id: 101, name: 'Felix', age: 31, city: 'TA' }]
 function strLength(str: string): number{
     return str.length;
 }
 // console.log(strLength(person.city));  //  -->  5
 
+/// =====  GENERICS =====
 
-
-
-  
-
-
+// interface Backpack<Type> {
+//     add: (obj: Type)=> void;
+//     get: ()=> Type;
+// }
+// declare const backpack: Backpack<string>;// if no 'declare' ERR -> "const' declarations must be initialized", used as a
+// const obj =backpack.get();               // shutter to  TS inlline compilation
+// // backpack.add(23); //since backpack variable is a string it will  pop an error
+//                      // "Argument of type 'number' is not assignable to parameter of type 'string'
 
 
 
