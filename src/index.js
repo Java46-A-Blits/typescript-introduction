2290ae7e2f933c561fff654a7207dae0266fdc04
Alex Blits <blits.alex@gmail.com>
2023-01-25 19:03:09 +0200

    test

---
+++ src/index.js
@@ -0,0 +1,2 @@
+const person1 = { id: 123, name: 'Moshe' };
+const person2 = { id: 1234, name: 'Sara' };
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+type Person = {
+    id: number,
+    name: string
+}
+const person1: Person = {id:123, name:'Moshe'};
+interface IPerson {
+    id: number,
+    name: string
+}
+const person2: IPerson = {id:1234, name:'Sara'};   
